91512d4485e7823b30b7de434c4bc0272c0a27af
jeffersonxavi <jefingbi2@gmail.com>
2022-02-16 09:25:24 -0300

    heroku

---
+++ app/Http/Requests/StoreCarroRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCarroRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+                'modelo' => 'required',
+
+                   
+        ];
+    }
+
+    public function messages(){
+        return [
+            'modelo' => 'Preencha o campo*',
+        ];
+    }
+}
