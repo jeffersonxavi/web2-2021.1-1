cad5a994ef1c63426ea45ccec3d14c35a73911a6
jeffersonxavi <jefingbi2@gmail.com>
2022-02-17 14:59:11 -0300

    att

--- resources/views/fornecedores/edit.blade.php
+++ resources/views/fornecedores/edit.blade.php
@@ -7,29 +7,36 @@
         @csrf
         @method('PUT')
         <div class="row">
-                <div class="col">
-                    <label for="nome">Nome do Fornecedor</label>
-                    <input type="text" class="form-control @error('nome') is-invalid @enderror" placeholder="Nome" name="nome" id="nome" value="{{$fornecedor->nome}}">
-                    @error('nome')
+            <div class="col">
+                <label for="nome">Nome do Fornecedor</label>
+                <input type="text" class="form-control @error('nome') is-invalid @enderror" placeholder="Nome" name="nome" id="nome" value="{{$fornecedor->nome}}">
+                @error('nome')
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>
                 @enderror
-                    <label for="email">E-mail do Fornecedor</label>
-                    <input type="text" class="form-control @error('email') is-invalid @enderror" placeholder="E-mail" name="email" id="email" value="{{$fornecedor->email}}">
-                    @error('email')
+                <label for="cnpj">CNPJ do Fornecedor</label>
+                <input type="text" class="form-control @error('cnpj') is-invalid @enderror" placeholder="CNPJ" name="CNPJ" id="cnpj" value="{{$fornecedor->cnpj}}">
+                @error('cnpj')
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>
                 @enderror
-                    <label for="endereco">Endereço do Fornecedor</label>
-                    <input type="text" class="form-control @error('endereco') is-invalid @enderror" placeholder="Endereço" name="endereco" id="endereco" value="{{$fornecedor->endereco}}">
-                    @error('endereco')
+                <label for="telefone">Telefone do Fornecedor</label>
+                <input type="text" class="form-control @error('telefone') is-invalid @enderror" placeholder="Telefone" name="telefone" id="telefone" value="{{$fornecedor->telefone}}">
+                @error('telefone')
                 <div class="invalid-feedback">
                     {{$message}}
                 </div>
                 @enderror
+                <label for="endereco">Endereço do Fornecedor</label>
+                <input type="text" class="form-control @error('endereco') is-invalid @enderror" placeholder="Endereço" name="endereco" id="endereco" value="{{$fornecedor->endereco}}">
+                @error('endereco')
+                <div class="invalid-feedback">
+                    {{$message}}
                 </div>
+                @enderror
+            </div>
         </div>
         <p><input class="btn btn-secondary active" type="submit" value="Salvar"></p>
     </form>
